c703f12b7ec458576c3a99fd8da2e4d84b320622
Javendean <javendeannaipaul@gmail.com>
2025-09-11 13:43:16 -0400

    Refactor generated SvelteKit files for improved formatting and structure
    - Updated internal.js to enhance readability and maintainability.
    - Reformatted non-ambient.d.ts for consistency in attribute definitions.
    - Adjusted tsconfig.json for better organization of compiler options and paths.
    - Cleaned up route_meta_data.json to streamline route definitions.
    - Enhanced type definitions in $types.d.ts files across various routes for clarity.
    - Improved action handling in proxy+page.server.js for the forgot-password route.
    - Standardized type definitions in update-password, contacts, and login routes.
    - Fixed formatting issues in multiple generated files to adhere to code style guidelines.
    - Updated .last-run.json to ensure proper formatting.

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -1,33 +1,37 @@
 export { matchers } from './matchers.js';
 
 export const nodes = [
-	() => import('./nodes/0'),
-	() => import('./nodes/1'),
-	() => import('./nodes/2'),
-	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5'),
-	() => import('./nodes/6')
+  () => import('./nodes/0'),
+  () => import('./nodes/1'),
+  () => import('./nodes/2'),
+  () => import('./nodes/3'),
+  () => import('./nodes/4'),
+  () => import('./nodes/5'),
+  () => import('./nodes/6'),
 ];
 
 export const server_loads = [0];
 
 export const dictionary = {
-		"/": [2],
-		"/auth/forgot-password": [~3],
-		"/auth/update-password": [4],
-		"/contacts": [~5],
-		"/login": [6]
-	};
-
-export const hooks = {
-	handleError: (({ error }) => { console.error(error) }),
-	
-	reroute: (() => {}),
-	transport: {}
+  '/': [2],
+  '/auth/forgot-password': [~3],
+  '/auth/update-password': [4],
+  '/contacts': [~5],
+  '/login': [6],
 };
 
-export const decoders = Object.fromEntries(Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]));
+export const hooks = {
+  handleError: ({ error }) => {
+    console.error(error);
+  },
+
+  reroute: () => {},
+  transport: {},
+};
+
+export const decoders = Object.fromEntries(
+  Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]),
+);
 
 export const hash = false;
 
--- .svelte-kit/types/src/routes/auth/forgot-password/proxy+page.server.js
+++ .svelte-kit/types/src/routes/auth/forgot-password/proxy+page.server.js
@@ -1,7 +1,11 @@
 // @ts-nocheck
 /** */
 export const actions = {
-  default:/** @param {import('./$types').RequestEvent} event */  async ({ request, url, locals: { supabase } }) => {
+  default: /** @param {import('./$types').RequestEvent} event */ async ({
+    request,
+    url,
+    locals: { supabase },
+  }) => {
     const formData = await request.formData();
     const email = formData.get('email');
 
