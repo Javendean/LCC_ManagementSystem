8b0ae2392ddabad8e37679fdf2819c511dc98d63
Javendean <javendeannaipaul@gmail.com>
2025-09-12 16:27:09 -0400

    feat: Add CSVUploader and new test case

--- my-app/src/lib/components/contacts-data-table/columns.js
+++ my-app/src/lib/components/contacts-data-table/columns.js
@@ -1,28 +1,26 @@
-import { createRender } from '@tanstack/svelte-table';
-
 /** @type {import('tanstack-table-8-svelte-5').ColumnDef<import('$lib/types').Contact>[]} */
 export const columns = [
   {
     accessorKey: 'first_name',
     header: 'First Name',
-    cell: (info) => createRender(info.getValue()),
+    cell: (info) => info.getValue(),
   },
   {
     accessorKey: 'last_name',
     header: 'Last Name',
-    cell: (info) => createRender(info.getValue()),
+    cell: (info) => info.getValue(),
   },
   {
     accessorKey: 'phone_number',
     header: 'Phone Number',
-    cell: (info) => createRender(info.getValue()),
+    cell: (info) => info.getValue(),
   },
   {
     accessorKey: 'created_at',
     header: 'Date Added',
     cell: (info) => {
       const date = new Date(info.getValue());
-      return createRender(date.toLocaleDateString('en-US'));
+      return date.toLocaleDateString('en-US');
     },
   },
 ];
