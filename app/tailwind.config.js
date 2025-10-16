d0abc743d1134c684123bb33da2b55f96635f5ca
Javendean <javendeannaipaul@gmail.com>
2025-10-16 15:42:48 -0400

    fix: Regenerate tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,11 @@
-import skeleton from '@skeletonlabs/tw-plugin';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    './index.html',
     './node_modules/@skeletonlabs/skeleton/**/*.svelte',
   ],
   theme: {
     extend: {},
   },
-  plugins: [skeleton],
+  plugins: [require('@skeletonlabs/tw-plugin')],
 }
